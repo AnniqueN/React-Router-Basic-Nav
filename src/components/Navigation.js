a77594160492cfae9eb01dd59a4019b4aa1f599c
Annique Nsabimana <anniquensabimana@gmail.com>
2019-07-23 18:32:43 -0500

    addedComments

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,3 +1,5 @@
+//imported {link} from react -router-dom'
+//declared to and removed a tag, replaced with link tag to specify the correct routes for the app to nagivigate towards
 import React from 'react';
 import { Link } from "react-router-dom";
 const Navigation = () => {
